40c1d06b384c5419a83e066b95efc7af0c9428fa
Manuel Luna <manuellunada@gmail.com>
2020-06-04 15:03:48 -0500

    Add Palette and ColorBox components

---
+++ src/components/ColorBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import "./ColorBox.css";
+
+export default class ColorBox extends Component {
+  render() {
+    return (
+      <div className="ColorBox" style={{ backgroundColor: this.props.color }}>
+        <span>{this.props.name}</span>
+        <span>More</span>
+      </div>
+    );
+  }
+}
