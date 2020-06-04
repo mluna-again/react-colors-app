3b76c7df4cec842455f174e4978ec85a90a1a56a
Manuel Luna <manuellunada@gmail.com>
2020-06-04 17:26:44 -0500

    Add copy-on-click with animations

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,21 +1,49 @@
 import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import "./ColorBox.css";
 
 export default class ColorBox extends Component {
+  state = {
+    isCopied: false,
+  };
+
+  handleCopy = () => {
+    this.setState({ isCopied: true }, () => {
+      setTimeout(() => this.setState({ isCopied: false }), 1500);
+    });
+  };
+
   render() {
     const { name, color } = this.props;
+    const overlayClasses = `ColorBox-copy-overlay ${
+      this.state.isCopied ? "active-overlay" : ""
+    }`;
+    const messageClasses = `ColorBox-copy-message ${
+      this.state.isCopied ? "active-message" : ""
+    }`;
 
     return (
-      <div className="ColorBox" style={{ backgroundColor: color }}>
-        <div className="ColorBox-copy-container">
-          <div className="ColorBox-content">
-            <span>{name}</span>
+      <CopyToClipboard text={color} onCopy={this.handleCopy}>
+        <div className="ColorBox" style={{ backgroundColor: color }}>
+          {/* Overlay starts here... */}
+          <div style={{ backgroundColor: color }} className={overlayClasses} />
+          <div className={messageClasses}>
+            <h1>Copied!</h1>
+            <p>{color}</p>
           </div>
-          <button className="ColorBox-copy-btn">Copy</button>
+          {/* Overlay ends here. */}
+
+          <div className="ColorBox-copy-container">
+            <div className="ColorBox-content">
+              <span>{name}</span>
+            </div>
+            <button className="ColorBox-copy-btn">Copy</button>
+          </div>
+
+          <span className="ColorBox-more">More</span>
         </div>
-        <span className="ColorBox-more">More</span>
-      </div>
+      </CopyToClipboard>
     );
   }
 }
