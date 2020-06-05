35762e005c5cdbea01ca6b8eb7b19b70a7d1b291
Manuel Luna <manuellunada@gmail.com>
2020-06-04 19:27:26 -0500

    Add slider

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import Palette from "./components/Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[2]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+
 import ColorBox from "./ColorBox";
 import "./Palette.css";
 
 export default class Palette extends Component {
+  state = {
+    level: 300,
+  };
+
+  handleChange = (level) => {
+    this.setState({ level });
+  };
+
   render() {
-    const colorBoxes = this.props.colors.map((color) => (
-      <ColorBox color={color.color} name={color.name} />
+    const colors = this.props.palette.colors[this.state.level];
+    const colorBoxes = colors.map((color) => (
+      <ColorBox color={color.hex} name={color.name} />
     ));
 
     return (
       <div className="Palette">
+        <Slider
+          defaultValue={this.state.level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={this.handleChange}
+        />
         {/* Navbar */}
         <div className="Palette-colors">{colorBoxes}</div>
         {/* Footer */}
