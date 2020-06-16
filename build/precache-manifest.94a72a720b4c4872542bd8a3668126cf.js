5d6cd453ea5f99624d5f0490aef648150d4f53f4
Manuel Luna <manuellunada@gmail.com>
2020-06-15 23:18:31 -0500

    Built for deploy

---
+++ build/precache-manifest.94a72a720b4c4872542bd8a3668126cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb50da3c65ae1cb15bd0a277493d9698",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b29744acb845e0b043dc",
+    "url": "/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "a595c5e90db9c2d2a161",
+    "url": "/static/css/main.163bad41.chunk.css"
+  },
+  {
+    "revision": "b29744acb845e0b043dc",
+    "url": "/static/js/2.e4e4aafa.chunk.js"
+  },
+  {
+    "revision": "8d15751606bc5f7eee9b708c985803de",
+    "url": "/static/js/2.e4e4aafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a595c5e90db9c2d2a161",
+    "url": "/static/js/main.0a99c364.chunk.js"
+  },
+  {
+    "revision": "96fa00f960854ef3d3ec",
+    "url": "/static/js/runtime-main.c5751b09.js"
+  }
+]);
